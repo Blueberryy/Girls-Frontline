454ea16a041826aba718f097155accc82347acaf
RukoSanada <ruko_sanada@yahoo.com>
2019-01-20 15:45:22 -0500

    Add files via upload

---
+++ girls-frontline/entities/entities/cw_ammo_357magnum/shared.lua
@@ -0,0 +1,13 @@
+ENT.Type = "anim"
+ENT.Base = "cw_ammo_ent_base"
+ENT.PrintName = ".357 Magnum Ammo"
+ENT.Author = "Spy"
+ENT.Spawnable = true
+ENT.AdminSpawnable = true 
+ENT.Category = "CW 2.0 Ammo"
+
+ENT.CaliberSpecific = true
+ENT.AmmoCapacity = 90
+ENT.ResupplyAmount = 30
+ENT.Caliber = ".357 Magnum"
+ENT.Model = "models/Items/BoxMRounds.mdl"
