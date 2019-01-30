72515042966d53e417560976a510348c4cfff31e
RukoSanada <ruko_sanada@yahoo.com>
2019-01-30 00:50:52 -0500

    Hand issue
    Conflicting issue with multiple hand calls, un-needed

--- girls-frontline/gamemode/cl_init.lua
+++ girls-frontline/gamemode/cl_init.lua
@@ -23,18 +23,6 @@ function brawl.debugmsg( text, ... )
 
 end
 
-function GM:PostDrawViewModel( vm, ply, weapon )
-
-	if ( weapon.UseHands || !weapon:IsScripted() ) then
-
-		local hands = LocalPlayer():GetHands()
-		if ( IsValid( hands ) ) then hands:DrawModel() end
-
-	end
-
-end
-
-
 function brawl.init()
 
 	brawl.loadConfig()
